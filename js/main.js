4bae56c7fc64566119968e79b2757ad6e4ff2467
Tatianos7 <balan.tv.20.05@gmail.com>
2024-11-20 19:00:41 +0300

    Добавляет валидацию

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-import './data.js';
+import { arrayPhotos } from './data.js';
 import './util.js';
+import './big-picture.js';
 import {renderThumbnails} from './thumbnails.js';
+import {initUploadModal} from './upload-photo-form.js';
 
-renderThumbnails();
+renderThumbnails(arrayPhotos);
+initUploadModal();
+
---
+++ js/upload-photo-form.js
@@ -0,0 +1,69 @@
+import { isEscapeKey } from "./util.js";
+
+// Находим классы
+const uploadForm = document.querySelector('.img-upload__form');
+const pageBody = document.querySelector('body');
+const uploadFileControl = uploadForm.querySelector('#upload-file'); // открытие формы
+const photoEditorForm = uploadForm.querySelector('.img-upload__overlay');
+const photoEditorResetBtn = photoEditorForm.querySelector('#upload-cancel');
+const hashtagInput = uploadForm.querySelector('.text__hashtags');
+const commentInput = uploadForm.querySelector('.text__description');
+
+// открытие/закрытие формы / загрузка фото
+
+const onPhotoEditorResetBtnClick = () => {
+  closePhotoEditor(); // вызов функции закрыия формы (колбэк)
+};
+
+const onDocumentKeydown = (evt) => {
+if (isEscapeKey(evt)) { // клавиша ESC
+evt.preventDefault(); // отменяем действие по умолчанию
+  uploadForm.reset(); // сбрасываем значение формы
+closePhotoEditor(); // вызыываем функцию закрытия формы
+}
+};
+// закрытие формы
+
+function closePhotoEditor () {
+  photoEditorForm.classList.add('hidden'); // доб.обратно класс
+  pageBody.classList.remove('.modal-open'); // удаляем класс
+  document.removeEventListener('keydown', onDocumentKeydown); // удаляет прослушиватель нажатия кнопки с документа
+  photoEditorResetBtn.removeEventListener('click', onPhotoEditorResetBtnClick); //удаляет прослушиватель клика кнопки
+  uploadFileControl.value = ''; //обнуляет файл
+}
+// открытие формы
+
+const initUploadModal = () => {
+  uploadFileControl.addEventListener('change', () => { // обработчик событий
+    photoEditorForm.classList.remove('hidden'); // удаление класса
+    pageBody.classList.add('.modal-open'); // на body вешаем класс
+    photoEditorResetBtn.addEventListener('click', onPhotoEditorResetBtnClick); // добавляем прослушиватель клика кнопки
+    document.addEventListener('keydown', onDocumentKeydown); //доб. прослушиватель нажатия кнопки с документа
+  });
+};
+
+// валидация хэштегов/комментов
+
+const pristine = new Pristine(uploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__field-wrapper--error',
+  errorTextParent: 'img-upload__field-wrapper',
+});
+const onFormSubmit = (evt) => { // логика проверки попытки отправить форму
+  evt.preventDefault();
+  if (pristine.validate()) {
+    hashtagInput.value = hashtagInput.value.trim().replaceAll(/\s+/g, '');
+    uploadForm.submit();
+  };
+};
+
+pristine.addValidator(hashtagInput, (value) => {
+  const hasNumber = /\d/.test(value);
+  return !hasNumber;
+}, 'Ошибка здесь');
+pristine.addValidator(commentInput, (value) => {
+  const comment = value.length <= 140;
+  return comment;
+}, 'Много букв, остановись');
+uploadForm.addEventListener('submit', onFormSubmit);
+export {initUploadModal};
